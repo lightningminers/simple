633af6ad5d4257a5eded163d855072878951a217
icepy <xiangwenwe@foxmail.com>
2019-03-30 00:38:07 +0800

    [update] 更新格式

--- src/i_1.ts
+++ src/i_1.ts
@@ -1,4 +1,5 @@
-/// 两数之和
+/// title: 两数之和
+/// URL: https://leetcode-cn.com/problems/two-sum/
 
 export const twoSumMap = (nums: number[], target: number): number[] | undefined => {
   let m = new Map();
